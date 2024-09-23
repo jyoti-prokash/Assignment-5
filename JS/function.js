945b4b638212c8f0993c5cc0e11742059bb5b342
jyotiprokash <jyotiprokash2000@gmail.com>
2024-09-24 00:47:31 +0600

    added donation and history button toggle

---
+++ JS/function.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+function donateInput(id){
+  const donateValue =parseFloat(document.getElementById(id).value);
+  return donateValue;
+}
+
+
+
+
+// Home Button to go homepage
+document.getElementById('home').addEventListener("click", function(){
+    // window.location.href='index.html';
+    window.location.href='index.html';
+});
